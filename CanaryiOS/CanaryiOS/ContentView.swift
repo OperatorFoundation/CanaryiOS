f773cdad25c60df243ebeffb3f2eb12f11dfff06
consuelita <adelita@operatorfoundation.org>
2022-06-09 16:28:17 -0500

    Use user selected folder

--- CanaryiOS/CanaryiOS/ContentView.swift
+++ CanaryiOS/CanaryiOS/ContentView.swift
@@ -8,119 +8,164 @@
 import SwiftUI
 import UniformTypeIdentifiers
 
-struct ContentView: View {
+struct ContentView: View
+{
     //Display Strings
-    let configTitle: String = "Transport load Config"
-    let configSearchPrompt = "Please enter the location of your Transport load Config"
+    let configTitle: String = "Transport config directory:"
+    let configSearchPrompt = "Please select the directory where your config files are located."
     let browseButtonTitle = "Browse"
-    let runTimePrompt = "How many times do you want to run the test?"
+    let numberOfRunsPrompt = "How many times do you want to run the test?"
     let runButtonTitle = "Run Test"
     let logTitle = "Run Log"
     let runTimes = " Time(s)"
-    //Constants
+    
     let step = 1
     let range = 1...10
-    //vars
-    @State private var configLocation = "No Config Loaded"
+
+    @State var configDirectory: URL = FileManager.default.temporaryDirectory
     @State private var numberOfRuns = 1
-    @State private var runLogs = " Lorem ipsum "
-    @State var shows = false
+    @State private var runLogs = ""
+    @State var showDirectoryPicker = false
     
     
     var body: some View
     {
         VStack(alignment: .center)
         {
-            Text(configTitle)
-            TextField("test", text: $configLocation) {}
-                .multilineTextAlignment(.center)
-                .foregroundColor(Color.red)
-
-            Button(action: { self.shows.toggle() })
+            VStack(alignment: .center, spacing: 10) // Config Directory UI
             {
-                Text("Select File")
-            }
-                .sheet(isPresented: self.$shows)
+                Text(configTitle)
+                Text(configDirectory.lastPathComponent).padding()
+                    .foregroundColor(.gray)
+
+                Button(action: { self.showDirectoryPicker.toggle() })
                 {
-                    DirectoryPickerView()
+                    Text(browseButtonTitle)
                 }
-
-            Spacer()
-                .frame(height: 55)
-        }
-        
-        VStack(alignment: .center)
-        {
-                Text(runTimePrompt)
-            ZStack(alignment: .center)
-            {
-                //Spacer(minLength:25)
-                Text(String(numberOfRuns)+runTimes)
-                Stepper(value: $numberOfRuns,
-                                in: range,
-                                step: step){}
-                Spacer(minLength:25)
-            }
-            Button(runButtonTitle)
-            {
-                //Run functionality
-                let canaryController = CanaryController()
-                
-                do
-                {
-                    let documentDirectory = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
-                    let directoryContents = try FileManager.default.contentsOfDirectory(at: documentDirectory, includingPropertiesForKeys: nil)
-                                
-                    if directoryContents.isEmpty
+                    .sheet(isPresented: self.$showDirectoryPicker)
                     {
-                        // TODO: For test purposes only, this is not a valid config.
-                        let testFileURL = documentDirectory.appendingPathComponent("emptyShadowConfig.json")
-                        let testFileContents = "{\"password\": \"abc123\", \"cipherName\": \"DarkStar\", \"serverIP\": \"0.0.0.0\", \"port\": 0000}"
-                        
-                        try testFileContents.write(to: testFileURL, atomically: true, encoding: .utf8)
+                        DirectoryPickerView(directoryURL: $configDirectory)
                     }
-                    
-                    canaryController.runCanary(configDirectory: documentDirectory, numberOfTimesToRun: numberOfRuns)
-                }
-                catch
+            }
+            .padding(10)
+            
+            Divider()
+            Spacer()
+            
+            VStack(alignment: .center) // Run Tests UI
+            {
+                Text(numberOfRunsPrompt)
+                Stepper(value: $numberOfRuns, in: range, step: step)
                 {
-                    // print("")
+                    Text(String(numberOfRuns)+runTimes)
+                }
+                    .padding(10)
+                Button(runButtonTitle)
+                {
+                    //Run functionality
+                    let canaryController = CanaryController()
+                    
+                    do
+                    {
+//                        let documentDirectory = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+//                        let directoryContents = try FileManager.default.contentsOfDirectory(at: documentDirectory, includingPropertiesForKeys: nil)
+//
+//                        if directoryContents.isEmpty
+//                        {
+//                            // TODO: For test purposes only, this is not a valid config.
+//                            let testFileURL = documentDirectory.appendingPathComponent("emptyShadowConfig.json")
+//                            let testFileContents = "{\"password\": \"abc123\", \"cipherName\": \"DarkStar\", \"serverIP\": \"0.0.0.0\", \"port\": 0000}"
+//
+//                            try testFileContents.write(to: testFileURL, atomically: true, encoding: .utf8)
+//                        }
+                        
+                        canaryController.runCanary(configDirectory: configDirectory, numberOfTimesToRun: numberOfRuns)
+                    }
+                    catch
+                    {
+                        // print("")
+                    }
+                }
+                .padding(10)
+            }
+            Divider()
+            Spacer()
+            
+            VStack(alignment: .center) // Logs UI
+            {
+                Text(logTitle)
+                ScrollView
+                {
+                   VStack
+                    {
+                        Text(runLogs)
+                            .lineLimit(nil)
+                    }
+                    .frame(maxWidth: .infinity)
                 }
             }
-            .buttonStyle(.bordered)
-        }
-        Spacer()
-            .frame(height: 40)
-        VStack(alignment: .center){
-            Text(logTitle)
-            ScrollView {
-                           VStack {
-                                Text(runLogs)
-                                     .lineLimit(nil)
-                           }.frame(maxWidth: .infinity)
-            }
-        }
-    }
-}
+            .padding()
+            
+        } // Main VStack
+    } // body
+} // ContentView
 
 struct DirectoryPickerView: UIViewControllerRepresentable
 {
-    func updateUIViewController(_ uiViewController: UIDocumentPickerViewController, context: Context) {
-        //
+    @Binding var directoryURL: URL
+    
+    func makeCoordinator() -> DocumentPickerCoordinator
+    {
+        return DocumentPickerCoordinator(directoryURL: $directoryURL)
     }
     
-    func makeUIViewController(context: Context) -> UIDocumentPickerViewController
+    func makeUIViewController(context: UIViewControllerRepresentableContext<DirectoryPickerView>) -> UIDocumentPickerViewController
     {
-        let documentPicker = UIDocumentPickerViewController(forOpeningContentTypes: [UTType.folder])
+        let controller: UIDocumentPickerViewController
         
-            return documentPicker
+        if #available(iOS 14, *)
+        {
+            controller = UIDocumentPickerViewController(forOpeningContentTypes: [.folder], asCopy: false)
+        }
+        else
+        {
+            controller = UIDocumentPickerViewController(documentTypes: [UTType.folder.identifier], in: .import)
+        }
+        
+        controller.delegate = context.coordinator
+        
+        return controller
+    }
+    
+    func updateUIViewController(_ uiViewController: UIDocumentPickerViewController, context: UIViewControllerRepresentableContext<DirectoryPickerView>) {
+        // Not used currently
     }
 }
 
+class DocumentPickerCoordinator: NSObject, UIDocumentPickerDelegate, UINavigationControllerDelegate
+{
+    @Binding var directoryURL: URL
+    
+    init(directoryURL: Binding<URL>)
+    {
+        _directoryURL = directoryURL
+    }
+    
+    // This is called when the user makes a selection
+    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL])
+    {
+        print(">> Document Picker didPickDocumentsAt called.")
+        if let selectedURL = urls.first
+        {
+            directoryURL = selectedURL
+            print("Selected url: \(selectedURL)")
+        }
+    }
 
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(configDirectory: FileManager.default.temporaryDirectory)
     }
 }
